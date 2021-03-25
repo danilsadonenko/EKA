957d7ea34bc80bac500dcc578929fde8fe9854c8
Данил Садоненко <danilsadonenko@mail.ru>
2021-03-25 15:27:46 +0300

    Финальное решение

---
+++ EKATestTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EKATestTask
+{
+    class Program
+    {
+        static byte[] array = { 1, 2, 3, 0, 1, 7, 8, 9 };
+        static readonly int M = array.Length;
+        static double result = 1;
+        static Action<byte[], int>[] methods = new Action<byte[], int>[] { loop, dummy };
+        static void loop(byte[] array, int i)
+        {
+            result *= array[i];
+            i++;
+            methods[i / M](array, i);
+        }
+        static void dummy(byte[] array, int i)
+        {
+        }
+        static void Main(string[] args)
+        {
+            loop(array, 0);
+            Console.WriteLine(new string[] { "yes", "no" }[(uint)(result / (result - 0.1))]);
+        }
+    }
+}
